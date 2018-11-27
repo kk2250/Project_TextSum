3e21aca37b248ff1dc120bd51b37c31083702e36
Kenny <xclen@hotmail.com>
2018-11-27 15:42:12 -0500

    tsets

--- tokenization_copy.py
+++ tokenization_copy.py
@@ -28,7 +28,7 @@ with open("dictionary.txt", "w") as dictionary:
 with open("dictionary_count.txt", "w") as dictionary1:
     dictionary1.write("{}".format(word_count))
 
-aff
+helloaff
 
 #if __name__ == '__main__':
 #   print(word_dict)
